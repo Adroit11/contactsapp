70cf7a020cfc872133262ee77d92d8145c89e270
msalom28 <manny.salom@gmail.com>
2016-12-04 15:26:00 -0500

    Update contacts repository interface

--- app/Http/Controllers/Api/ApiControllerInterface.php
+++ app/Http/Controllers/Api/ApiControllerInterface.php
@@ -9,6 +9,9 @@ interface ApiControllerInterface
 	public function index();
 	public function show($id);
 	public function store(Request $request);
+	public function update(Request $request);
+	public function delete(Request $request, $uid);
+	public function refresh();
 	public function respondNotSaved($message);
 	public function respond($data, $headers = []);
 
